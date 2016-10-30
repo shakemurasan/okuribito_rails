aeefdfae5a9adbe5899c38119fda85e634d491f2
ym <kiyoshi.kodama@gmail.com>
2016-10-31 00:24:20 +0900

    メソッド呼び出し検出時、モデルに登録するように修正。
    メソッド呼び出し検出時、モデルに登録する #8

--- lib/okuribito_recorder/railtie.rb
+++ lib/okuribito_recorder/railtie.rb
@@ -3,10 +3,13 @@ module OkuribitoRecorder
 
   class Railtie < ::Rails::Railtie
     config.after_initialize do
-      okuribito = Okuribito::OkuribitoPatch.new do |method_name, obj_name, caller_info|
-        puts "#{obj_name} #{method_name} #{caller_info[0]}"
+      okuribito = Okuribito::OkuribitoPatch.new do |method_name, _obj_name, caller_info, class_name, method_symbol|
+        MethodCallLog.create(class_name: class_name,
+                             method_symbol: method_symbol,
+                             method_name: method_name,
+                             back_trace: caller_info[0])
       end
-      okuribito.apply("config/okuribito.yml")
+      okuribito.apply("config/okuribito.yml") if File.exist?("config/okuribito.yml")
     end
   end
 end
