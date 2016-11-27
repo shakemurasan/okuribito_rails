723514277d997a824942d2aee4dcf5959e3e7d55
ym <kiyoshi.kodama@gmail.com>
2016-11-27 19:02:29 +0900

    メソッド呼び出し状況の表示(ページネーション) #10

--- app/controllers/okuribito_recorder/method_call_logs_controller.rb
+++ app/controllers/okuribito_recorder/method_call_logs_controller.rb
@@ -3,7 +3,7 @@ require_dependency "okuribito_recorder/application_controller"
 module OkuribitoRecorder
   class MethodCallLogsController < ApplicationController
     def index
-      @method_call_logs = MethodCallLog.all
+      @method_call_logs = MethodCallLog.page(params[Kaminari.config.param_name]).per(20)
     end
   end
 end
