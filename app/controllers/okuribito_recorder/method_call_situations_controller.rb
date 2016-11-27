6296f5b44b3c386f73f32f8b0c0690115427b3c3
ym <kiyoshi.kodama@gmail.com>
2016-11-27 18:51:00 +0900

    メソッド監視状況の表示(ページネーション) #13

--- app/controllers/okuribito_recorder/method_call_situations_controller.rb
+++ app/controllers/okuribito_recorder/method_call_situations_controller.rb
@@ -3,7 +3,7 @@ require_dependency "okuribito_recorder/application_controller"
 module OkuribitoRecorder
   class MethodCallSituationsController < ApplicationController
     def index
-      @method_call_situations = MethodCallSituation.page(params[:page]).per(20)
+      @method_call_situations = MethodCallSituation.page(params[Kaminari.config.param_name]).per(20)
     end
   end
 end
