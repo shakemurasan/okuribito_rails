812f5fb341f88d463e038118c35846427abd89ae
ym <kiyoshi.kodama@gmail.com>
2016-11-29 01:21:06 +0900

    メソッド監視状況にorderをかける #31

--- app/controllers/okuribito_rails/method_call_situations_controller.rb
+++ app/controllers/okuribito_rails/method_call_situations_controller.rb
@@ -3,7 +3,10 @@ require_dependency "okuribito_rails/application_controller"
 module OkuribitoRails
   class MethodCallSituationsController < ApplicationController
     def index
-      @method_call_situations = MethodCallSituation.page(params[Kaminari.config.param_name]).per(20)
+      @method_call_situations = MethodCallSituation
+                                .page(params[Kaminari.config.param_name])
+                                .per(20)
+                                .order(class_name: :asc, method_symbol: :asc, method_name: :asc)
     end
   end
 end
