65923550f01298fedb282171ebaeaf20ca1476ee
ym <kiyoshi.kodama@gmail.com>
2016-11-28 21:56:11 +0900

    ブロック内でガードのやり方がわからないので、ifで囲っておく

--- lib/okuribito_rails/railtie.rb
+++ lib/okuribito_rails/railtie.rb
@@ -4,16 +4,16 @@ require "okuribito_rails/observe_method"
 module OkuribitoRails
   class Railtie < ::Rails::Railtie
     config.after_initialize do
-      return unless ActiveRecord::Base.connection.table_exists? 'okuribito_rails_method_call_situations'
+      if ActiveRecord::Base.connection.table_exists? 'okuribito_rails_method_call_situations'
+        yaml_path = OkuribitoRails.config.setting_path
 
-      yaml_path = OkuribitoRails.config.setting_path
+        # Update database by observed methods.
+        RegistMethod.new.update_observe_methods(yaml_path)
 
-      # Update database by observed methods.
-      RegistMethod.new.update_observe_methods(yaml_path)
-
-      # Define behavior that when method called.
-      okuribito = ObserveMethod.new.patch_okuribito
-      okuribito.apply(yaml_path) if File.exist?(yaml_path)
+        # Define behavior that when method called.
+        okuribito = ObserveMethod.new.patch_okuribito
+        okuribito.apply(yaml_path) if File.exist?(yaml_path)
+      end
     end
   end
 end
