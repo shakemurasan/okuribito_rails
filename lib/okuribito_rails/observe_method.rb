ae87fd5cf5f29ab6beeb86804c43376d6a65aba5
ym <kiyoshi.kodama@gmail.com>
2016-12-19 01:46:06 +0900

    SELECT文からtransactionをかけるように修正

--- lib/okuribito_rails/observe_method.rb
+++ lib/okuribito_rails/observe_method.rb
@@ -6,15 +6,17 @@ module OkuribitoRails
       Okuribito::OkuribitoPatch.new(
         once_detect: config_once_detect
       ) do |method_name, _obj_name, caller_info, class_name, method_symbol|
-        situation = MethodCallSituation.find_by(class_name: class_name,
-                                                method_symbol: method_symbol,
-                                                method_name: method_name)
-        if situation.present?
-          MethodCallLog.create(method_call_situation: situation,
-                               class_name: class_name,
-                               method_symbol: method_symbol,
-                               method_name: method_name,
-                               back_trace: caller_info[0])
+        ActiveRecord::Base.transaction do
+          situation = MethodCallSituation.find_by(class_name: class_name,
+                                                  method_symbol: method_symbol,
+                                                  method_name: method_name)
+          if situation.present?
+            MethodCallLog.create(method_call_situation: situation,
+                                 class_name: class_name,
+                                 method_symbol: method_symbol,
+                                 method_name: method_name,
+                                 back_trace: caller_info[0])
+          end
         end
       end
     end
