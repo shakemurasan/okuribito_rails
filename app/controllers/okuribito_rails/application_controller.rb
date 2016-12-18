f3e331c2cc23a48dba1584f8caea81fdc3b0ac36
ym <kiyoshi.kodama@gmail.com>
2016-12-18 22:09:58 +0900

    RAILS_ENV によって、OkuribitoRailsが動かないようにする #41

--- app/controllers/okuribito_rails/application_controller.rb
+++ app/controllers/okuribito_rails/application_controller.rb
@@ -1,5 +1,24 @@
 module OkuribitoRails
   class ApplicationController < ActionController::Base
     protect_from_forgery
+
+    if Gem::Version.new(Rails.version) < Gem::Version.new(4)
+      before_filter :available_action!
+    else
+      before_action :available_action!
+    end
+
+    def available_action!
+      if prohibited_env?
+        render nothing: true, status: 403
+        return
+      end
+    end
+
+    private
+
+    def prohibited_env?
+      OkuribitoRails.config.prohibit_webui.include?(ENV["RAILS_ENV"])
+    end
   end
 end
