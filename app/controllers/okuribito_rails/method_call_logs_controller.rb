bb3cf534ad1cf859e5f6eb303cd70c0facc2f430
ym <kiyoshi.kodama@gmail.com>
2016-12-25 09:29:39 +0900

    Implementation of search function. メソッド呼び出し状況の表示(検索機能) #11

--- app/controllers/okuribito_rails/method_call_logs_controller.rb
+++ app/controllers/okuribito_rails/method_call_logs_controller.rb
@@ -3,7 +3,15 @@ require_dependency "okuribito_rails/application_controller"
 module OkuribitoRails
   class MethodCallLogsController < ApplicationController
     def index
-      @method_call_logs = MethodCallLog.page(params[Kaminari.config.param_name]).per(20)
+      @method_call_logs = MethodCallLog
+                          .search(params.presence || {})
+                          .page(params[Kaminari.config.param_name])
+    end
+
+    private
+
+    def method_call_log_params
+      params.permit(:class_name, :method_name)
     end
   end
 end
--- app/models/okuribito_rails/method_call_log.rb
+++ app/models/okuribito_rails/method_call_log.rb
@@ -5,5 +5,15 @@ module OkuribitoRails
     validates :class_name, presence: true, length: { minimum: 1, maximum: 255 }
     validates :method_symbol, presence: true, inclusion: { in: %w(. #) }
     validates :method_name, presence: true, length: { minimum: 1, maximum: 255 }
+
+    scope :with_class_name, ->(class_name) { where(class_name: class_name) }
+    scope :with_method_name, ->(method_name) { where(method_name: method_name) }
+
+    def self.search(args)
+      mcl = self
+      mcl = mcl.with_class_name(args[:class_name]) if args[:class_name].present?
+      mcl = mcl.with_method_name(args[:method_name]) if args[:method_name].present?
+      mcl
+    end
   end
 end
