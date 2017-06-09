5bf1f2db9e280e02859fa7c16fc7084161926a83
武尾 健太 Kenta Takeo <takeo@pepabo.com>
2017-06-09 15:18:10 +0900

    Fix ActionController::UnpermittedParameters found unpermitted parameter: page

--- app/controllers/okuribito_rails/method_call_situations_controller.rb
+++ app/controllers/okuribito_rails/method_call_situations_controller.rb
@@ -4,15 +4,21 @@ module OkuribitoRails
   class MethodCallSituationsController < ApplicationController
     def index
       @method_call_situations = MethodCallSituation
-                                .search(method_call_situation_params.presence || {})
+                                .search(search_params)
                                 .page(params[Kaminari.config.param_name])
                                 .order(class_name: :asc, method_symbol: :asc, method_name: :asc)
     end
 
     private
 
-    def method_call_situation_params
-      params.permit(:class_name, :method_name, :x_days_passed, :uncalled_method, :called_method)
+    def search_params
+      params.slice(
+        :class_name,
+        :method_name,
+        :x_days_passed,
+        :uncalled_method,
+        :called_method
+      ).presence || {}
     end
   end
 end
